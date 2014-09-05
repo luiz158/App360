b361fd5be402c8f21ce310e6fc76555912d9a04f
Peter Falken <luis@bitamins.net>
2014-09-05 10:23:05 -0500

    Code cleanup regarding Batch User Provisioning. Testing button with class "centered tcentered".

--- src/main/java/com/bitjester/apps/common/login/LoginManager.java
+++ src/main/java/com/bitjester/apps/common/login/LoginManager.java
@@ -1,7 +1,6 @@
 package com.bitjester.apps.common.login;
 
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -10,7 +9,6 @@ import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
-import com.bitjester.apps.cfa.hhrr.entities.Employee;
 import com.bitjester.apps.common.entities.AppUser;
 import com.bitjester.apps.common.utils.BookKeeper;
 import com.bitjester.apps.common.utils.FacesUtil;
@@ -19,9 +17,6 @@ import com.bitjester.apps.common.utils.HashUtil;
 @Named
 @Stateless
 public class LoginManager {
-	@Inject
-	private String appName;
-
 	@Inject
 	EntityManager em;
 
@@ -81,28 +76,6 @@ public class LoginManager {
 		user.setPassword(HashUtil.calc_HashSHA("123456"));
 		user.setMustChangePassword(Boolean.TRUE);
 		em.merge(user);
-		em.flush();
-	}
-
-	// ==== Batch user provisioning based on evaluators
-	public void provisionUsers() throws Exception {
-		String query = "SELECT DISTINCT(ev.evaluator) FROM Evaluation ev";
-		query += " WHERE ev.evaluator.active = TRUE AND ev.evaluator.doc_id NOT IN";
-		query += " (SELECT DISTINCT(username) FROM User)";
-		Iterator<Employee> ite = em.createQuery(query, Employee.class).getResultList().iterator();
-
-		Employee emp;
-		AppUser user;
-		while (ite.hasNext()) {
-			emp = ite.next();
-			user = new AppUser();
-			user.setEmployee(emp);
-			user.setName(emp.getFullName());
-			user.setPassword(HashUtil.calc_HashSHA("123456"));
-			user.setUsername(emp.getDoc_id());
-			user.setAppRole(appName, "user");
-			em.persist(user);
-		}
 		em.flush();
 	}
 }
